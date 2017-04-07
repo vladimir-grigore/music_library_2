5eb2224a96db92364aff48d1e90ff9f4e3e019cd
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-07 22:06:28 +0000

    Add chalk library for color output in console.log

--- music_library.js
+++ music_library.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 class Library {
   constructor(name, creator){
     this.name = name;
@@ -7,6 +9,9 @@ class Library {
   addPlaylist(playlist) {
     this.playlists.push(playlist);
   }
+  showPlaylists(){
+    console.log(this.playlists);
+  }
 }
 
 class Playlist {
@@ -14,15 +19,36 @@ class Playlist {
     this.name = name;
     this.tracks = [];
   }
-  overallRating();
-  totalDuration();
-}
-
-class Track {
-  constructor(title, rating, length){
-    this.title = title;
-    this.rating = rating;
-    this.length = length;
+  overallRating(){};
+  totalDuration(){};
+  showTracks(){
+    console.log(this.tracks);
   }
 }
 
+class Track {
+  constructor(title, artist, rating, length){
+    this.title = title;
+    this.artist = artist;
+    this.rating = rating;
+    this.length = length;
+  }
+  showDetails(){
+    console.log(`Title: ${chalk.green(this.title)}
+                Artist: ${chalk.green(this.artist)}
+                Rating: ${chalk.yellow(this.rating)}
+                Length: ${chalk.yellow(this.length)}`);
+  }
+}
+
+var heyLove = new Track("Hey Love", "The Delfonics", 5, 200);
+var shineOn = new Track("Shine On You Crazy Diamond", "Pink Floyd", 5, 810);
+var breakOn = new Track("Break On Through", "The Doors", 4, 150);
+var zephyr  = new Track("The Zephyr Song", "Red Hot Chili Peppers", 3, 332);
+var crimson = new Track("The Court of the Crimson King", "King Crimson", 4, 460);
+
+heyLove.showDetails();
+shineOn.showDetails();
+breakOn.showDetails();
+zephyr.showDetails();
+crimson.showDetails();
